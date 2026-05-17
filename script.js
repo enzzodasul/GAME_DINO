a726f00166e773ad79d1fd93bc6f45fb0cdb63b6
enzzodasul <enzzodasul@gmail.com>
2026-05-17 16:01:55 -0300

    versão final funcionando obsaculos em mvimento e sensor de colisao

--- script.js
+++ script.js
@@ -1,75 +1,117 @@
-document.addEventListener('DOMContentLoaded',() => {
-   //referencias
+document.addEventListener('DOMContentLoaded', () => {
+
+    //referencias
     const dino = document.querySelector('.dino')
     const grid = document.querySelector('.grid')
-    const body = document.querySelector('.body')
-    const alert = document.querySelector('.alert')            
-   
+    const body = document.querySelector('body')
+    const alert = document.getElementById('alert')
+
     //variaveis
     let jumping = false
     let gravity = 0.9
     let gameo = false
     let dinopy = 0
 
-//Entrada de dados
-document.addEventListener('keyup',jumpcontrol)
+    //Entrada de dados
+    document.addEventListener('keyup', jumpcontrol)
 
     //controle de pulo
-    function jumpcontrol(e){
-        if(e.keyCode == 32){
-            if(!jumping){
-                jumping= true
+    function jumpcontrol(e) {
+
+        if (e.keyCode == 32) {
+
+            if (!jumping) {
+                jumping = true
                 jump()
             }
         }
     }
 
-    function jump(){
+    function jump() {
+
         let count = 0
-        let timerId = setInterval(function(){
+
+        let timerId = setInterval(function () {
+
             //caindo
-            if(count == 15){
+            if (count == 15) {
+
                 clearInterval(timerId)
-                let downTimerId = setInterval(function(){
-                    if(count == 0){
+
+                let downTimerId = setInterval(function () {
+
+                    if (count == 0) {
+
                         clearInterval(downTimerId)
                         jumping = false
                     }
+
                     dinopy -= 5
-                    count --
+                    count--
+
                     dinopy = dinopy * gravity
                     dino.style.bottom = dinopy + 'px'
-                },20)
 
-            
+                }, 20)
+
             }
 
             //subida
-            dinopy +=30
+            dinopy += 30
             count++
+
             dinopy = dinopy * gravity
             dino.style.bottom = dinopy + 'px'
-        },20)
+
+        }, 20)
     }
 
-    function gerarobst(){
-        let random = Math.random()*4000
+    function gerarobst() {
+
+        let random = Math.random() * 4000
         let obstaclepx = 1000
+
         const obstacle = document.createElement('div')
 
         //criando copias
-        if(!gameo) obstacle.classList.add('obstacle')
-            grid.appendChild(obstacle)
+        if (!gameo)
+            obstacle.classList.add('obstacle')
+
+        grid.appendChild(obstacle)
+
         obstacle.style.left = obstaclepx + 'px'
 
         //logica do jogo + movimento dos obstaculos
-        let timerId = setInterval(function(){
-            obstaclepx -= 10
-            obstacle.style.left = obstaclepx = 'px'
-        },20)
+        let timerId = setInterval(function () {
 
+            //colisao com player
+            if (obstaclepx > 0 && obstaclepx < 60 && dinopy < 60) {
+
+                clearInterval(timerId)
+
+                alert.innerHTML = 'Fim de Jogo'
+
+                gameo = true
+
+                //removendo copias
+                body.removeChild(body.firstChild)
+
+                while (grid.firstChild) {
+                    grid.removeChild(grid.lastChild)
+                }
+            }
+
+            //movimento dos cactos para a esquerda
+            obstaclepx -= 10
+            obstacle.style.left = obstaclepx + 'px'
+
+        }, 20)
+
+        if (!gameo)
+            setTimeout(gerarobst, random)
 
     }
 
-})
+    gerarobst()
 
+})
